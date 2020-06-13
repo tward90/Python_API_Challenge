2060a4fc1cad8d7dfc02cc6cbc6a88abaa08a2fc
Col D <60669354+tward90@users.noreply.github.com>
2020-06-13 07:46:29 -0500

    WeatherPy file checkpoint - convert all city data to DF.

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4cbcc12ce30bffba7f6e2c06651408fc"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBnjl6hEZkeysrI7VgM0dHTd3jSNgczagE"
